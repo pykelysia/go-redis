0bf87f910c68acc72e59022dfe404b1ed0a5e9b3
pyke <pykelysia0206@163.com>
2025-09-30 16:28:50 +0800

    add(string-opt): 完成 string-opt 部分

--- base-opt/string-opt/main.go
+++ base-opt/string-opt/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	defer client.Close()
+
+	client.Set(ctx, "key", "value", 60)
+	res, err := client.Get(ctx, "key").Result()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("key: ", res)
+}
